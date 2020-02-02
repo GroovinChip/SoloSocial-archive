b0afa485fb8b2e7a77431e8eb44ea1d470bb1e73
GroovinChip <groovinchip@gmail.com>
2020-02-01 21:34:11 -0500

    update getting current user in certain widgets

--- lib/screens/post_feed.dart
+++ lib/screens/post_feed.dart
@@ -1,6 +1,13 @@
 import 'package:solo_social/library.dart';
 
 class PostFeed extends StatefulWidget {
+  final FirebaseUser user;
+
+  const PostFeed({
+    Key key,
+    @required this.user,
+  }) : super(key: key);
+
   @override
   _PostFeedState createState() => _PostFeedState();
 }
@@ -10,110 +17,98 @@ class _PostFeedState extends State<PostFeed> {
 
   @override
   Widget build(BuildContext context) {
-    final _userBloc = Provider.of<UserBloc>(context);
-    return StreamBuilder<FirebaseUser>(
-        stream: _userBloc.currentUser,
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Theme.of(context).canvasColor,
+        title: Text(
+          'Posts',
+          style: GoogleFonts.openSans(
+            fontSize: 26,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+      body: StreamBuilder<QuerySnapshot>(
+        stream: _users.document(widget.user.uid).collection('Posts').snapshots(),
         builder: (context, snapshot) {
           if (!snapshot.hasData) {
-            return Scaffold(
-              body: Center(
-                child: CircularProgressIndicator(),
-              ),
+            return Center(
+              child: CircularProgressIndicator(),
             );
           } else {
-            final _user = snapshot.data;
-            return Scaffold(
-              appBar: AppBar(
-                elevation: 0,
-                backgroundColor: Theme.of(context).canvasColor,
-                title: Text(
-                  'Posts',
-                  style: GoogleFonts.openSans(
-                    fontSize: 26,
-                    fontWeight: FontWeight.bold,
+            final _posts = snapshot.data.documents;
+            if (_posts.length == 0 || _posts == null) {
+              return Center(
+                child: Text(
+                  'No Posts',
+                  style: TextStyle(
+                    color: Colors.white,
+                  ),
+                ),
+              );
+            } else {
+              return ListView.builder(
+                itemCount: _posts.length,
+                padding: EdgeInsets.only(right: 8, left: 8),
+                itemBuilder: (context, index) {
+                  final _post = _posts[index];
+                  var _tags;
+                  if (_post['Tags'] != null) {
+                    _tags = (jsonDecode(_post['Tags']) as List).cast<String>();
+                  }
+                  DateTime _timeCreated = DateTime.parse(_post['TimeCreated']);
+
+                  return PostCard(
+                    user: widget.user,
+                    timeCreated: _timeCreated,
+                    postId: _post.documentID,
+                    username: _post['Username'],
+                    postText: _post['PostText'],
+                    tags: _tags == null || _tags.length == 0 ? [] : _tags,
+                    sourceLink: _post['SourceLink'].toString().isEmpty ? 'NoSource' : _post['SourceLink'],
+                  );
+                },
+              );
+            }
+          }
+        },
+      ),
+      floatingActionButton: ComposeFab(),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      bottomNavigationBar: BottomAppBar(
+        color: Theme.of(context).primaryColor,
+        child: Container(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              IconButton(
+                icon: Icon(Icons.menu),
+                onPressed: () => showModalBottomSheet(
+                  context: context,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.only(
+                      topRight: Radius.circular(12),
+                      topLeft: Radius.circular(12),
+                    ),
+                  ),
+                  backgroundColor: Theme.of(context).canvasColor,
+                  builder: (_) => MainMenuSheet(
+                    user: widget.user,
                   ),
                 ),
               ),
-              body: StreamBuilder<QuerySnapshot>(
-                stream: _users.document(_user.uid).collection('Posts').snapshots(),
-                builder: (context, snapshot) {
-                  if (!snapshot.hasData) {
-                    return Center(
-                      child: CircularProgressIndicator(),
-                    );
-                  } else {
-                    final _posts = snapshot.data.documents;
-                    if (_posts.length == 0 || _posts == null) {
-                      return Center(
-                        child: Text(
-                          'No Posts',
-                          style: TextStyle(
-                            color: Colors.white,
-                          ),
-                        ),
-                      );
-                    } else {
-                      return ListView.builder(
-                        itemCount: _posts.length,
-                        padding: EdgeInsets.only(right: 8, left: 8),
-                        itemBuilder: (context, index) {
-                          final _post = _posts[index];
-                          var _tags;
-                          if (_post['Tags'] != null) {
-                            _tags = (jsonDecode(_post['Tags']) as List).cast<String>();
-                          }
-                          DateTime _timeCreated = DateTime.parse(_post['TimeCreated']);
-
-                          return PostCard(
-                            user: _user,
-                            timeCreated: _timeCreated,
-                            postId: _post.documentID,
-                            username: _post['Username'],
-                            postText: _post['PostText'],
-                            tags: _tags == null || _tags.length == 0 ? [] : _tags,
-                            sourceLink: _post['SourceLink'].toString().isEmpty ? 'NoSource' : _post['SourceLink'],
-                          );
-                        },
-                      );
-                    }
-                  }
-                },
-              ),
-              floatingActionButton: ComposeFab(),
-              floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-              bottomNavigationBar: BottomAppBar(
-                color: Theme.of(context).primaryColor,
-                child: Container(
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: <Widget>[
-                      IconButton(
-                        icon: Icon(Icons.menu),
-                        onPressed: () => showModalBottomSheet(
-                          context: context,
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.only(
-                              topRight: Radius.circular(12),
-                              topLeft: Radius.circular(12),
-                            ),
-                          ),
-                          backgroundColor: Theme.of(context).canvasColor,
-                          builder: (_) => MainMenuSheet(),
-                        ),
-                      ),
-                      /*IconButton(
+              /*IconButton(
                         icon: Icon(Icons.search),
                         onPressed: () => showSearch(
                           context: context,
                           delegate: PostSearch(),
                         ),
                       ),*/
-                    ],
-                  ),
-                ),
-              ),
-            );
-          }
-        });
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
--- lib/widgets/main_menu_sheet.dart
+++ lib/widgets/main_menu_sheet.dart
@@ -3,10 +3,14 @@ import 'package:solo_social/library.dart';
 // ignore: must_be_immutable
 class MainMenuSheet extends StatelessWidget {
   final FirebaseAuth _auth = FirebaseAuth.instance;
+  final FirebaseUser user;
+
+  MainMenuSheet({
+    @required this.user,
+  });
 
   @override
   Widget build(BuildContext context) {
-    final _userBloc = Provider.of<UserBloc>(context);
     return Theme(
       data: ThemeData.dark(),
       child: Container(
@@ -17,39 +21,44 @@ class MainMenuSheet extends StatelessWidget {
               padding: const EdgeInsets.all(8),
               child: ModalDrawerHandle(),
             ),
-            StreamBuilder<FirebaseUser>(
-                stream: _userBloc.currentUser,
-                builder: (context, snapshot) {
-                  if (!snapshot.hasData) {
-                    return CircularProgressIndicator();
-                  } else {
-                    final user = snapshot.data;
-                    return ListTile(
-                      leading: CircleAvatar(
-                        backgroundColor: Colors.white,
-                        backgroundImage: NetworkImage(user.photoUrl),
-                      ),
-                      title: Text(user.displayName),
-                      subtitle: Text(user.email),
-                      trailing: FlatButton(
-                        child: Text('Sign Out'),
-                        onPressed: () {
-                          _auth.signOut();
-                          Navigator.of(context).pushAndRemoveUntil(
-                            MaterialPageRoute(
-                              builder: (context) => Login(),
-                            ), (route) => false,
-                          );
-                        },
-                      ),
-                    );
-                  }
-                }
-            ),
             ListTile(
-              leading: Icon(Icons.file_download),
-              title: Text('Download Posts'),
-              onTap: () {},
+              leading: CircleAvatar(
+                backgroundColor: Colors.white,
+                backgroundImage: NetworkImage(user.photoUrl),
+              ),
+              title: Text(user.displayName),
+              subtitle: Text(user.email),
+              trailing: FlatButton(
+                child: Text('Sign Out'),
+                onPressed: () {
+                  _auth.signOut();
+                  Navigator.of(context).pushAndRemoveUntil(
+                    MaterialPageRoute(
+                      builder: (context) => Login(),
+                    ),
+                    (route) => false,
+                  );
+                },
+              ),
+            ),
+            StreamBuilder<QuerySnapshot>(
+              stream: Firestore.instance.collection('Users').document(user.uid).collection('Posts').snapshots(),
+              builder: (context, snapshot) {
+                if (!snapshot.hasData) {
+                  return CircularProgressIndicator();
+                } else {
+                  final _posts = snapshot.data.documents;
+                  return ListTile(
+                    leading: Icon(Icons.file_download),
+                    title: Text('Download Posts'),
+                    onTap: () {
+                      /*for (DocumentSnapshot post in _posts) {
+
+                      }*/
+                    },
+                  );
+                }
+              },
             ),
             /*ListTile(
               leading: Icon(Icons.alternate_email),
